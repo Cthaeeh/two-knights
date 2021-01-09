1344bb936a729d408fefa9eec456fa405cc90476
Kai <u7moench@gmail.com>
2021-01-09 15:53:25 +0100

    Add HUnit dependency

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Test.HUnit
+
+
+testOne :: Test
+testOne = TestCase $ assertEqual "Some Message" (Just 1) (Just 1) 
+
+testTwo :: Test
+testTwo = TestCase $ assertEqual "Other Message" (Just 1) Nothing
+
+
+main :: IO Counts
+main = runTestTT $ TestList [testOne, testTwo]
